9fa82497a74145563b9f28045b08f531cb024e5a
pt657407064 <taopan1211@gmail.com>
2017-11-18 02:04:25 -0500

    Add delete operations for binary search tree

--- data structure/Tree.java
+++ data structure/Tree.java
@@ -1,86 +1,141 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tree;
+public class BinaryTree {
 
-/**
- *
- * @author taopa
- */
-public class Tree {
-    
     Node root;
-    
+
     public void insert(Node node){
         if(root == null){
             root = node;
-        }    
+        }
         else{
             Node focusNode = root;
             Node parent;
-           while(true){
-               parent = focusNode;
-               if(node.key < focusNode.key){
-                   focusNode = focusNode.LeftChild;
-                   if(focusNode == null){
-                       parent.LeftChild = node;
-                       return;
-                   }
-               }else{
-                   focusNode = focusNode.RightChild;
-                   if(focusNode==null){
-                       parent.RightChild = node;
-                       return;
-                   }
-               }
-           }      
+            while(true){
+                parent = focusNode;
+                if(node.key < focusNode.key){
+                    focusNode = focusNode.LeftChild;
+                    if(focusNode == null){
+                        parent.LeftChild = node;
+                        return;
+                    }
+                }else{
+                    focusNode = focusNode.RightChild;
+                    if(focusNode==null){
+                        parent.RightChild = node;
+                        return;
+                    }
+                }
+            }
         }
-    } 
+    }
+    
+    public void deleteNode(int key){
+        if(root == null) return;
+        if(root.key == key){
+            if(root.LeftChild== null && root.RightChild == null) root = null;
+            else if(root.LeftChild != null && root.RightChild == null) root = root.LeftChild;
+            else if(root.LeftChild == null && root.RightChild != null) root = root.RightChild;
+            else {
+                Node tem = root.LeftChild;
+                root = root.RightChild;
+                Node focusNode = root;
+                while(focusNode.LeftChild != null){
+                    focusNode = focusNode.LeftChild;
+                }
+                focusNode.LeftChild = tem;
+            }
+        }
+        Node focusNode = root;
+        Node parent = null;
+
+        while(focusNode != null){
+            if(focusNode.key == key){
+                if(key < parent.key) {
+                    if (focusNode.LeftChild == null && focusNode.RightChild == null) parent.LeftChild = null;
+                    else if (focusNode.LeftChild == null && focusNode.RightChild != null)
+                        parent.LeftChild = focusNode.RightChild;
+                    else if (focusNode.LeftChild != null && focusNode.RightChild == null)
+                        parent.LeftChild = focusNode.RightChild;
+                    else {
+                        Node tem = focusNode.LeftChild;
+                        focusNode = focusNode.RightChild;
+                        parent.LeftChild = focusNode;
+                        while(focusNode.LeftChild !=null){
+                            focusNode = focusNode.LeftChild;
+                        }
+                        focusNode.LeftChild = tem;
+                    }
+                }
+                else{
+                    if(focusNode.LeftChild == null && focusNode.LeftChild == null) parent.RightChild = null;
+                    else if (focusNode.LeftChild == null && focusNode.RightChild != null)
+                        parent.RightChild = focusNode.RightChild;
+                    else if(focusNode.LeftChild!=null && focusNode.RightChild == null) parent.RightChild =focusNode.LeftChild;
+                    else{
+                        Node tem = focusNode.LeftChild;
+                        focusNode = focusNode.RightChild;
+                        parent.LeftChild = focusNode;
+                        while(focusNode.LeftChild !=null){
+                            focusNode = focusNode.LeftChild;
+                        }
+                        focusNode.LeftChild = tem;
+                    }
+                }
+            }
+            else if(key > focusNode.key){
+                parent = focusNode;
+                focusNode = focusNode.RightChild;
+            }
+            else{
+                parent = focusNode;
+                focusNode = focusNode.LeftChild;
+            }
+        }
+
+
+    }
     public void displayInOrderTraverse(Node node){
         if(node != null){
-        displayInOrderTraverse(node.LeftChild);
-        System.out.println(node);
-        displayInOrderTraverse(node.RightChild);
+            displayInOrderTraverse(node.LeftChild);
+            System.out.println(node);
+            displayInOrderTraverse(node.RightChild);
         }
     }
-     public void displayPreOrderTraverse(Node node){
+    public void displayPreOrderTraverse(Node node){
         if(node != null){
-        System.out.println(node);
-        displayPreOrderTraverse(node.LeftChild);
-        displayPreOrderTraverse(node.RightChild);
-        }
-    }   
-    
-      public void displayPostOrderTraverse(Node node){
-        if(node != null){
-        displayPostOrderTraverse(node.LeftChild);
-        displayPostOrderTraverse(node.RightChild);
-        System.out.println(node);
+            System.out.println(node);
+            displayPreOrderTraverse(node.LeftChild);
+            displayPreOrderTraverse(node.RightChild);
         }
     }
-    public Node search(Node node){
+
+    public void displayPostOrderTraverse(Node node){
+        if(node != null){
+            displayPostOrderTraverse(node.LeftChild);
+            displayPostOrderTraverse(node.RightChild);
+            System.out.println(node);
+        }
+    }
+    public Node search(int key){
         Node focusNode = root;
         while(focusNode != null){
-            if(focusNode.key == node.key)
+            if(focusNode.key == key)
             {
                 return focusNode;
             }
             else{
-                if(focusNode.key > node.key){
+                if(focusNode.key > key){
                     focusNode = focusNode.LeftChild;
                 }
                 else{
                     focusNode = focusNode.RightChild;
                 }
-                    
+
             }
         }
         return null;
     }
     public static void main(String[] args) {
-        Tree t = new Tree();
+        BinaryTree t = new BinaryTree();
         t.insert(new Node(50,"Boss"));
         t.insert(new Node(100,"manager"));
         t.insert(new Node(75,"vp"));
@@ -95,11 +150,13 @@ public class Tree {
         t.displayPreOrderTraverse(t.root);
         System.out.println();
         t.displayPostOrderTraverse(t.root);
-        
-        System.out.println("Found"+ t.search(new Node(25,"developer")));
-        
+        System.out.println();
+        t.deleteNode(50);
+        t.displayPostOrderTraverse(t.root);
+        System.out.println("Found "+ t.search(50));
+
     }
-    
+
 }
 class Node{
     Node LeftChild;
@@ -108,38 +165,6 @@ class Node{
     String value;
     public Node(int key, String value){
         this.key = key;
-        this.value = value;
-    }
-
-    public Node getLeftChild() {
-        return LeftChild;
-    }
-
-    public void setLeftChild(Node LeftChild) {
-        this.LeftChild = LeftChild;
-    }
-
-    public Node getRightChild() {
-        return RightChild;
-    }
-
-    public void setRightChild(Node RightChild) {
-        this.RightChild = RightChild;
-    }
-
-    public int getKey() {
-        return key;
-    }
-
-    public void setKey(int key) {
-        this.key = key;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
         this.value = value;
     }
     public String toString(){
