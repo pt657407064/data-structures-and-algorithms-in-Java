78ccf6435e8dbaba9557d6e9c626ee5daac01be2
pt657407064 <taopan1211@gmail.com>
2018-03-16 01:22:43 -0400

    Queue Bug fixed

--- data structure/Queue.java
+++ data structure/Queue.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package queue;
-
 import java.util.Arrays;
 
 /**
@@ -13,7 +6,6 @@ import java.util.Arrays;
  */
 public class Queue<E> {
 
-    
     int front,rear, numberOfItem = 0;
     Object [] queue;
     int size = 0;
@@ -24,41 +16,40 @@ public class Queue<E> {
     public void enqueue(E element){
         if(numberOfItem +1 <= size){
             queue[rear] = element;
+            System.out.println(queue[rear] + " added");
             if(rear == size -1) //So it will not have stackoverflow
-                rear = 0;
-            rear++;           
+                rear = -1;//Go back to the -1 because we have front ++ follow to make it to index 0
+            rear++;
             numberOfItem++;
-            System.out.println("Element added");
+
         }
-        
     }
     public E peek(){
         return (E)queue[front];
     }
     public void dequeue(){
-        System.out.println("Element Remove");
         if(numberOfItem > 0){
+        System.out.println(queue[front].toString() +" Removeed");
         queue[front] = null;
         if(front == size-1) //So it will not have stackoverflow
-            front = 0;
+            front = -1; //Go back to the -1 because we have front ++ follow to make it to index 0
         front ++;
         numberOfItem --;
         }
     }
     public static void main(String[] args) {
-      Queue<String> queue = new Queue<>(10);
-      while(true){
+      Queue<String> queue = new Queue<>(2);
+      h: while(true){
           queue.enqueue("Apple");
           queue.enqueue("Facebook");
+          queue.dequeue();
+          queue.dequeue();
           queue.enqueue("Google");
           queue.enqueue("Amazon");
           queue.enqueue("Oracle");
           queue.dequeue();
           queue.dequeue();
           queue.dequeue();
-          queue.dequeue();
-          queue.dequeue();
-          
           queue.enqueue("Microsoft");
           queue.enqueue("AirBandB");
           queue.enqueue("Linkedin");
@@ -68,9 +59,10 @@ public class Queue<E> {
           queue.dequeue();
           queue.dequeue();
           queue.dequeue();
-          
+          break h;
+
       }
-      
+
     }
-    
+
 }
